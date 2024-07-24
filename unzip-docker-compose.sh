4370d856e6a57e3730b1a8d865d9fd2835a91182
eunsung <eunsung.shin@petfins.net>
2024-07-24 12:07:48 +0900

    update shell script;

--- unzip-docker-compose.sh
+++ unzip-docker-compose.sh
@@ -6,8 +6,5 @@ for tar_file in docker_images/*.tar; do
   docker load -i "$tar_file"
 done
 
-cp docker_images/docker-compose.yaml ./docker-compose.yaml
-cp docker_images/unzip-docker-compose.sh ./unzip-docker-compose.sh
-
 # Docker Compose 실행
 docker-compose up -d
