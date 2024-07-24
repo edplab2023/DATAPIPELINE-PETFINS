8b4dd59a9f9d4a8284fb7709a79a3b880acf59f8
eunsung <eunsung.shin@petfins.net>
2024-07-24 15:14:15 +0900

    이미지 빌드 오류 수정

--- unzip-docker-compose.sh
+++ unzip-docker-compose.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Docker 이미지 로드
-for tar_file in *.tar; do
-  echo "Loading $tar_file..."
-  docker load -i "$tar_file"
-done
+docker load -i images.tar
+
 
 # Docker Compose 실행
 docker-compose up -d
--- zip-docker-compose.sh
+++ zip-docker-compose.sh
@@ -7,32 +7,6 @@ docker-compose build
 # 서비스 이름 목록 가져오기
 services=$(docker-compose config --services)
 
-# 이미지 tar 파일로 저장
-mkdir -p docker_images
-
-# 이미지 목록 확인
-docker images
-
-for service in $services; do
-  # 서비스에 대한 이미지 이름 가져오기
-  image_name=$(docker-compose config | awk '/image:/{print $2}' | grep -E "^${service}\b" | head -n 1)
-
-  if [ -z "$image_name" ]; then
-    # 이미지 이름이 없는 경우 서비스 이름을 이미지 이름으로 설정
-    image_name=${service}
-  fi
-
-  # 이미지 ID 가져오기
-  image_id=$(docker images -q $image_name)
-
-  if [ -n "$image_id" ]; then
-    echo "Saving image for service $service as $image_name.tar"
-    docker save -o docker_images/${image_name}.tar $image_id
-  else
-    echo "Image id = $image_id"
-    echo "No image found for service $service"
-  fi
-done
 
 # 2. Docker Compose 파일 준비
 COMPOSE_FILE="docker-compose.yaml"
@@ -40,12 +14,15 @@ UNZIP_FILE="unzip-docker-compose.sh"
 ENV_FILE=".env"
 
 # 3. 모든 파일을 .zip으로 압축
-IMAGES_DIR="docker_images/"
+IMAGES_DIR="docker_images"
 ZIP_FILE="docker_project.zip"
 
-cp $COMPOSE_FILE $IMAGES_DIR$COMPOSE_FILE
-cp $UNZIP_FILE $IMAGES_DIR$UNZIP_FILE
-cp $ENV_FILE $IMAGES_DIR$ENV_FILE
+mkdir -p $IMAGES_DIR
+docker save $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'datapipeline') -o $IMAGES_DIR/images.tar
+
+cp $COMPOSE_FILE $IMAGES_DIR/$COMPOSE_FILE
+cp $UNZIP_FILE $IMAGES_DIR/$UNZIP_FILE
+cp $ENV_FILE $IMAGES_DIR/$ENV_FILE
 
 echo "Compressing files into $ZIP_FILE..."
 zip -r $ZIP_FILE $IMAGES_DIR
